ac5e5ba51362d42dd818d2ace9456faeea60c1ff
大石头 <nnhy@vip.qq.com>
2017-06-05 10:59:14 +0800

    随机生成符合全天时间的数据

--- BigData/Program.cs
+++ BigData/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Big.Data.Entity;
 using NewLife.Log;
@@ -48,12 +44,7 @@ namespace BigData
             var batch = 10000;
 
             // 模拟时序数据
-            //var time = new DateTime(2000, 1, 1);
             var time = DateTime.Now.Date;
-            if (count > 0) time = SalesOrder.FindAll(null, SalesOrder._.ID.Desc(), null, 0, 1)[0].CreateTime;
-
-            // 时间均摊到某一天，随机产生，几率加倍
-            var add = (Int32)((24.0 * 3600 * 1000 / total) * 2);
 
             var stat = new Statistics();
             Task task = null;
@@ -77,12 +68,11 @@ namespace BigData
 
                 var sd = new SalesOrder();
                 //sd.Number = Rand.Next().ToString().PadLeft(12, '0');
-                sd.Number = i.ToString().PadLeft(12, '0');
+                sd.Number = (i + 1).ToString().PadLeft(12, '0');
                 sd.NodeID = Rand.Next(1, 32);
                 //sd.NodeID = 1;
 
-                //time = time.AddMilliseconds(Rand.Next(0, add));
-                time = time.AddMilliseconds(add / 2);
+                time = time.AddMilliseconds(Rand.Next(0, 24 * 3600 * 1000));
                 sd.CreateTime = time;
                 //sd.Insert();
                 list.Add(sd);
