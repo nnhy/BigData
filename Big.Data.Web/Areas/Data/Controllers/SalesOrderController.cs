03310505a45aa7ad0390cffeb934ce82619891b8
大石头 <nnhy@vip.qq.com>
2018-07-16 00:01:20 +0800

    魔方用法调整

--- Big.Data.Web/Areas/Data/Controllers/SalesOrderController.cs
+++ Big.Data.Web/Areas/Data/Controllers/SalesOrderController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 using Big.Data.Entity;
 using NewLife.Cube;
@@ -8,17 +9,25 @@ namespace Big.Data.Web.Areas.Data.Controllers
 {
     public class SalesOrderController : EntityController<SalesOrder>
     {
-        /// <summary>列表页视图。子控制器可重载，以传递更多信息给视图，比如修改要显示的列</summary>
-        /// <param name="p"></param>
-        /// <returns></returns>
-        protected override ActionResult IndexView(Pager p)
+        ///// <summary>列表页视图。子控制器可重载，以传递更多信息给视图，比如修改要显示的列</summary>
+        ///// <param name="p"></param>
+        ///// <returns></returns>
+        //protected override ActionResult IndexView(Pager p)
+        //{
+        //    // 禁止非索引字段排序
+        //    if (!p.Sort.EqualIgnoreCase("", "ID", "Number")) p.Sort = null;
+
+        //    var list = SalesOrder.Search(p["dtStart"].ToDateTime(), p["dtEnd"].ToDateTime(), p["Q"], p);
+
+        //    return View("List", list);
+        //}
+
+        protected override IEnumerable<SalesOrder> Search(Pager p)
         {
             // 禁止非索引字段排序
             if (!p.Sort.EqualIgnoreCase("", "ID", "Number")) p.Sort = null;
 
-            var list = SalesOrder.Search(p["dtStart"].ToDateTime(), p["dtEnd"].ToDateTime(), p["Q"], p);
-
-            return View("List", list);
+            return SalesOrder.Search(p["dtStart"].ToDateTime(), p["dtEnd"].ToDateTime(), p["Q"], p);
         }
     }
 }
